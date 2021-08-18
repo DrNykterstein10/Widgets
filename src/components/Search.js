5f08a4551aaee6aef5cdea871f060e55bb47148d
Piyush Rai <piyush.rai.official@gmail.com>
2021-08-18 23:44:11 +0530

    Implemented search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
     
     const [searchTerm, setSearchTerm] = useState('');
+    const [results, setResults] = useState([]);
+    
+
+    useEffect(() => {
+        const search = async () => {
+            const {data} = await axios.get('https://en.wikipedia.org/w/api.php',{
+                params : {
+                    action : 'query',
+                    list : 'search',
+                    origin : '*',
+                    format : 'json',
+                    srsearch : searchTerm
+                }
+            });
+            setResults(data.query.search);
+        };
+
+        if(searchTerm && !results.length){
+            search();
+        } else{
+            const timeoutId = setTimeout(() => {
+                if(searchTerm){
+                    search();
+                }
+            }, 500);
+    
+            return () => {
+                clearTimeout(timeoutId);
+            };
+        }
+
+    }, [searchTerm]);
+
+    const renderedList = results.map((result) => {
+        return (
+            <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a className="ui button"
+                       href = {`https://en.wikipedia.org?curid=${result.pageid}`}
+                    >
+                        Read
+                    </a>
+                </div>
+                <div className="content">
+                    <div className="header">{result.title}</div>
+                    <span dangerouslySetInnerHTML={{__html : result.snippet}}></span>
+                </div>
+            </div>
+        );
+    });
 
     return (
         <div className="ui segment">
@@ -15,6 +66,9 @@ const Search = () => {
                     className="input">
                     </input>
                 </div>
+            </div>
+            <div className="ui celled list">
+                {renderedList}
             </div>
         </div>
     );
